e7bf942a7e320e0bf9d042a0fecb8a29b7e03806
Kevin Allard <kallard@carnegielearning.com>
2025-07-21 17:12:50 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// This file holds configuration variables for the editor.
+// WARNING: This key will be visible in the browser's developer tools.
+// Do not use this method for production applications with sensitive keys.
+const TINY_MCE_API_KEY = 'tth7kpsyc53fbzgd6zz6kz2huhefefwxbts007tf2zx7ab4t';
